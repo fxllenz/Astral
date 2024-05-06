5fb131e14eaec28ed974bfd8d686042002485822
fxllout <168219048+fxllout@users.noreply.github.com>
2024-05-06 13:28:41 +0100

    Updated Logger

--- src/events/msgDelete.js
+++ src/events/msgDelete.js
@@ -15,7 +15,7 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setColor('Red')
                 .setDescription(messageContent);
-            const webhookURL = 'https://discord.com/api/webhooks/1236455752885993534/5j8Pnh-5Kplaugmv8FTovHlbPZBKgj6mTj9Pj3_jBIoSWxlr7Q6ufxx5LslwtmaX8JgG';
+            const webhookURL = 'https://discord.com/api/webhooks/';
             const webhook = new WebhookClient({ url: webhookURL });
             webhook.send({ embeds: [embed] })
                 .catch(error => console.error('Error sending deleted message log:', error));

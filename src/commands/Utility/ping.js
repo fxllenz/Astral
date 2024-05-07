dc69cae5f694d34b2f345447997793ba934e6e8e
fxllout <vaypt@proton.me>
2024-05-08 00:55:24 +0100

    Added /ping

---
+++ src/commands/Utility/ping.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription(`Get The Bots Latency`)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const { client, options, member } = interaction;
+        const embed = new EmbedBuilder()
+            .setColor('DarkBlue')
+            .setTitle('Astral Network Stats')
+            .setDescription(`> **API Latency:** ${client.ws.ping}ms\n`)
+            .setTimestamp();
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: false
+        });
+    },
+};

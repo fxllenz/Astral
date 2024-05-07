0a8e241a58c544d3a486bd768939be1e3cf19981
fxllout <vaypt@proton.me>
2024-05-08 00:56:05 +0100

    Added /userinfo

---
+++ src/commands/Utility/userinfo.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Displays detailed information about the user.')
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription(`The users avatar to fetch`)
+            .setRequired(false)
+        ),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const embed = new EmbedBuilder()
+            .setColor('DarkBlue')
+            .setTitle(`${user.username}'s Info`)
+            .setDescription(`**Username:** ${user.username}\n**User ID:** ${user.id}\n**Display Name:** ${user.displayName}\n**User Mention:** <@${user.id}>\n**Joined Discord:** ${user.createdAt ? user.createdAt.toUTCString() : 'Unknown'}\n**Joined Server:** ${user.joinedAt ? user.joinedAt.toUTCString() : 'Unknown'}`)
+            .setThumbnail(user.displayAvatarURL())
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
